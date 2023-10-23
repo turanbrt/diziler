d82f682392fe287d1859333499efe51eb5780799
turan <mturan.barut@gmail.com>
2023-10-24 01:42:31 +0300

    c#101-diziler

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace diziler;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Dizi tanımlama
+    /*    
+        string[] renkler=new string[5];
+        string[] hayvanlar={"kedi" , "kopek","kus"};
+        int[] dizi;
+        dizi=new int[5];
+
+        //dizilere değer atama
+        renkler[0]="mavi";
+        
+        dizi[3]=10;
+
+        Console.WriteLine(renkler[0]);
+        Console.WriteLine(hayvanlar[0]);
+        Console.WriteLine(dizi[3]);
+*/
+        //diziler ile döngü
+        //klavyeden girilen n tane sayının ortalamasını hesaplayan program
+        Console.WriteLine("Dizinin eleman sayısını giriniz.");
+        int diziUzunlugu=int.Parse(Console.ReadLine());
+        int[] sayiDizisi=new int[diziUzunlugu];
+        for(int i=0;    i<diziUzunlugu; i++){
+            Console.WriteLine(i+1 +". sayiyi giriniz:");
+            sayiDizisi[i]=int.Parse(Console.ReadLine());
+
+        }
+        int toplam=0;
+
+        foreach (var sayi in sayiDizisi)
+        {   
+            toplam=toplam+sayi;
+            
+        }
+        Console.WriteLine("Ortalama:"+toplam/diziUzunlugu);
+        
+    }
+}
+ 
